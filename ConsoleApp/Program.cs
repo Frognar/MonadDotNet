3700683a919a3f56e4ce012efef035dd8eaf4e51
Frognar <sebastian.przyszlak@gmail.com>
2023-05-29 21:50:25 +0200

    Use Logged monad

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,5 +7,13 @@ int result = 2
 
 Maybe<int> AddOne(int x) => x + 1;
 Maybe<int> Square(int x) => x * x;
-
 Console.WriteLine(result);
+
+Logged<int> loggedResult = 2
+  .RunWithLogs(AddOneWithLog)
+  .RunWithLogs(SquareWithLog);
+
+Logged<int> AddOneWithLog(int x) => Logged<int>.Of(x + 1, $"Added 1 to {x} to get {x + 1}");
+Logged<int> SquareWithLog(int x) => Logged<int>.Of(x * x, $"Squared {x} to get {x * x}");
+Console.WriteLine(loggedResult.Value);
+Console.WriteLine(string.Join(Environment.NewLine, loggedResult.Logs));
