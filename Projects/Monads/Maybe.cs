682ff209b316b2b6c9113edb083cdd79c1a07e48
Frognar <sebastian.przyszlak@gmail.com>
2023-05-29 21:37:45 +0200

    Create Maybe monad

---
+++ Projects/Monads/Maybe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Frognar.Monads {
+  public readonly struct Maybe<T> {
+    public Maybe(T value) => Value = value;
+    public T Value { get; }
+    public bool HasValue => Value != null;
+    public static Maybe<T> None { get; } = new Maybe<T>();
+    public static Maybe<T> Of(T value) => new Maybe<T>(value);
+
+    public static implicit operator Maybe<T>(T value) => Of(value);
+  }
+
+  public static class MaybeExtensions {
+    public static Maybe<U> Run<T, U>(this T value, Func<T, Maybe<U>> func) => Maybe<T>.Of(value).Run(func);
+    public static Maybe<U> Run<T, U>(this Maybe<T> value, Func<T, Maybe<U>> func) => value.HasValue ? func(value.Value) : Maybe<U>.None;
+  }
+}
