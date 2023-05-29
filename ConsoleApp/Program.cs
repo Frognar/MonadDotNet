6e027bb9cdc7c4ab2a0448aa863b1aeb1a04e4f4
Frognar <sebastian.przyszlak@gmail.com>
2023-05-29 21:38:09 +0200

    Use Maybe monad

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Frognar.Monads;
+
+int result = 2
+  .Run(AddOne)
+  .Run(Square)
+  .Value;
+
+Maybe<int> AddOne(int x) => x + 1;
+Maybe<int> Square(int x) => x * x;
+
+Console.WriteLine(result);
