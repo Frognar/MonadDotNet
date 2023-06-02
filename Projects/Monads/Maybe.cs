c9a9b6de4fcd1fe0492aec2e9dcb2fecccbb933c
Frognar <sebastian.przyszlak@gmail.com>
2023-06-02 14:15:29 +0200

    Test creating Maybe with value

--- Projects/Monads/Maybe.cs
+++ Projects/Monads/Maybe.cs
@@ -1,5 +1,13 @@
 ﻿namespace Frognar.Monads; 
 
 public readonly struct Maybe<T> {
+  public bool HasValue => true;
+  public int Value => 5;
+  
+  Maybe(int value) {
+  }
+
+  public static Maybe<int> From(int value) {
+    return new Maybe<int>(value);
   }
 }
---
+++ Tests/Monads.Tests.Unit/MaybeTests.cs
@@ -0,0 +1,15 @@
+﻿using Frognar.Monads;
+
+namespace Monads.Tests.Unit; 
+
+public class MaybeTests {
+  [Fact]
+  public void MaybeWithValue_ShouldHaveValue() {
+    const int validValue = 5;
+
+    Maybe<int> maybe = Maybe<int>.From(validValue);
+      
+    Assert.True(maybe.HasValue);
+    Assert.Equal(validValue, maybe.Value);
+  }
+}
