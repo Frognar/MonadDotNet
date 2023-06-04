3cd0ebac6decfd08a7323508efdaa097269945bf
Frognar <sebastian.przyszlak@gmail.com>
2023-06-04 06:26:21 +0200

    Test creating Ok Result

---
+++ Projects/Monads/Result.cs
@@ -0,0 +1,13 @@
+﻿namespace Frognar.Monads; 
+
+public readonly struct Result<T> {
+  public T Value { get; }
+  public bool IsSuccess { get; }
+  
+  Result(T value) {
+    Value = value;
+    IsSuccess = true;
+  }
+
+  public static Result<T> Ok(T value) => new(value);
+}
---
+++ Tests/Monads.Tests.Unit/ResultTests.cs
@@ -0,0 +1,14 @@
+﻿using Frognar.Monads;
+
+namespace Monads.Tests.Unit;
+
+public class ResultTests {
+  [Fact]
+  public void ReturnsSuccessWhenValueIsProvided() {
+    const string value = "test value";
+    Result<string> result = Result<string>.Ok(value);
+
+    Assert.True(result.IsSuccess);
+    Assert.Equal(value, result.Value);
+  }
+}
