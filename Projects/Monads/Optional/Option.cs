86604d71acbe97f08a56057e665e109b13c98d91
Frognar <sebastian.przyszlak@gmail.com>
2023-11-30 22:23:53 +0100

    Implement IEquatable<Option<T>> in Option<T>

--- Projects/Monads/Optional/Option.cs
+++ Projects/Monads/Optional/Option.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Frognar.Monads.Optional;
 
-public struct Option<T> where T : class {
+public struct Option<T> : IEquatable<Option<T>> where T : class {
   T? value;
 
   Option(T? value) {
@@ -14,5 +14,17 @@ public struct Option<T> where T : class {
   
   public Option<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class {
     return value is null ? Option<TResult>.None : Option<TResult>.Some(map(value));
+  }
+
+  public bool Equals(Option<T> other) {
+    return value?.Equals(other.value) ?? other.value is null;
+  }
+
+  public override bool Equals(object? obj) {
+    return obj is Option<T> other && Equals(other);
+  }
+
+  public override int GetHashCode() {
+    return value?.GetHashCode() ?? 0;
   }
 }
