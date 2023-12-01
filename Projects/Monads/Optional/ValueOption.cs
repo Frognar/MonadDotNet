4927ded5897a1eaa9f4797b140e7b120ecab956f
Frognar <sebastian.przyszlak@gmail.com>
2023-12-01 05:54:00 +0100

    Add ValueOption struct in Monads Optional

---
+++ Projects/Monads/Optional/ValueOption.cs
@@ -0,0 +1,14 @@
+﻿namespace Frognar.Monads.Optional;
+
+public readonly struct ValueOption<T> where T : struct
+{
+    private readonly T? value;
+
+    ValueOption(T? value)
+    {
+        this.value = value;
+    }
+
+    public static ValueOption<T> Some(T obj) => new(obj);
+    public static ValueOption<T> None => new(null);
+}
