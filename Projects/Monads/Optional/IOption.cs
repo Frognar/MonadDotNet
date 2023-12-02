217dc70f8cdd631d3884091e80fbecdaa10c2d94
frognar <sebastian.przyszlak@gmail.com>
2023-12-02 20:58:43 +0100

    Add IOption interface in the Optional namespace

---
+++ Projects/Monads/Optional/IOption.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Frognar.Monads.Optional;
+
+public interface IOption<T>
+{
+    IOption<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class;
+    IOption<TResult> MapValue<TResult>(Func<T, TResult> map) where TResult : struct;
+    IOption<TResult> FlatMap<TResult>(Func<T, IOption<TResult>> map) where TResult : class;
+    IOption<TResult> FlatMapValue<TResult>(Func<T, IOption<TResult>> map) where TResult : struct;
+    T Reduce(T defaultValue);
+    T Reduce(Func<T> defaultValue);
+    IOption<T> Where(Func<T, bool> predicate);
+    IOption<T> WhereNot(Func<T, bool> predicate);
+}
