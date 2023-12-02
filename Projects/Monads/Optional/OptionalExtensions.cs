8b3b44d67e69211ab8c1bc151927b9b8dc4dce17
frognar <sebastian.przyszlak@gmail.com>
2023-12-02 10:29:03 +0100

    Add ValueOption methods for struct in OptionalExtensions

--- Projects/Monads/Optional/OptionalExtensions.cs
+++ Projects/Monads/Optional/OptionalExtensions.cs
@@ -15,4 +15,16 @@ public static class OptionalExtensions
     public static Option<T> WhereNot<T>(this T? obj, Func<T, bool> predicate) where T : class {
         return obj is not null && !predicate(obj) ? Option<T>.Some(obj) : Option<T>.None();
     }
+    
+    public static ValueOption<T> ToOption<T>(this T? obj) where T : struct {
+        return obj.HasValue == false ? ValueOption<T>.None() : ValueOption<T>.Some(obj.Value);
+    }
+
+    public static ValueOption<T> Where<T>(this T? obj, Func<T, bool> predicate) where T : struct {
+        return obj.HasValue && predicate(obj.Value) ? ValueOption<T>.Some(obj.Value) : ValueOption<T>.None();
+    }
+
+    public static ValueOption<T> WhereNot<T>(this T? obj, Func<T, bool> predicate) where T : struct {
+        return obj.HasValue && !predicate(obj.Value) ? ValueOption<T>.Some(obj.Value) : ValueOption<T>.None();
+    }
 }
