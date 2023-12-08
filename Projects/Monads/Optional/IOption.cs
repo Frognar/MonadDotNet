bc274a0e1af18d0cda97e44cb3a795a307614e24
Frognar <sebastian.przyszlak@gmail.com>
2023-12-08 23:37:15 +0100

    Add MapAsync method to IOption interface

--- Projects/Monads/Optional/IOption.cs
+++ Projects/Monads/Optional/IOption.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Threading.Tasks;
 
 namespace Frognar.Monads.Optional;
 
 public interface IOption<T> {
   IOption<TResult> Map<TResult>(Func<T, TResult> map) where TResult : class;
+  Task<IOption<TResult>> MapAsync<TResult>(Func<T, Task<TResult>> map) where TResult : class;
   IOption<TResult> MapValue<TResult>(Func<T, TResult> map) where TResult : struct;
   IOption<TResult> FlatMap<TResult>(Func<T, IOption<TResult>> map) where TResult : class;
   IOption<TResult> FlatMapValue<TResult>(Func<T, IOption<TResult>> map) where TResult : struct;
