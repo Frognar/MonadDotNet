be0cf8a824b944fb903ab7dbaa739408194cff26
Frognar <sebastian.przyszlak@gmail.com>
2023-12-18 14:10:38 +0100

    Rename WriterResult to Writer

---
+++ Projects/Monads/Writers/Writer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Frognar.Monads.Writers;
+
+public readonly struct Writer<T, TLog> {
+  public T Value { get; }
+  public IEnumerable<TLog> Logs { get; }
+
+  public Writer(T value, IEnumerable<TLog> logs) {
+    Value = value;
+    Logs = logs;
+  }
+}
