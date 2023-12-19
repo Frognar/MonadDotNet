fe32f183e906328d4a77cbbcca062eea2b79364c
frognar <sebastian.przyszlak@gmail.com>
2023-12-19 12:35:14 +0100

    Create WriterExtensions class with WithLogs extension method

---
+++ Projects/Monads/Writers/WriterExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Frognar.Monads.Writers;
+
+public static class WriterExtensions {
+  public static Writer<T, TLog> WithLogs<T, TLog>(this T value, IEnumerable<TLog> logs) {
+    return Writer<T, TLog>.Wrap(value, logs.ToImmutableList());
+  }
+}
