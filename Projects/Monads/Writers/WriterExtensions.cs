82451ef98fe0c1433905ecda7a9c7495be417a6d
frognar <sebastian.przyszlak@gmail.com>
2023-12-20 06:56:55 +0100

    Add WithLogs extension method that create Writer with single log

--- Projects/Monads/Writers/WriterExtensions.cs
+++ Projects/Monads/Writers/WriterExtensions.cs
@@ -8,6 +8,10 @@ public static class WriterExtensions {
     return Writer<T, TLog>.Wrap(value, logs.ToImmutableList());
   }
   
+  public static Writer<T, TLog> WithLogs<T, TLog>(this T value, TLog log) {
+    return Writer<T, TLog>.Wrap(value, log);
+  }
+  
   public static Writer<T, TLog> WithLogs<T, TLog>(this T value) {
     return Writer<T, TLog>.Wrap(value);
   }
