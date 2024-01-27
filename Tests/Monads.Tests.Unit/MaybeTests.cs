04ec0083ec32191af52605719992246a3131d9a6
frognar <sebastian.przyszlak@gmail.com>
2024-01-27 07:12:22 +0100

    Rename test

--- Tests/Monads.Tests.Unit/MaybeTests.cs
+++ Tests/Monads.Tests.Unit/MaybeTests.cs
@@ -5,7 +5,7 @@ namespace Monads.Tests.Unit;
 
 public class MaybeTests {
   [Fact]
-  public void CreatingMaybeWithExistingValue() {
+  public void ReturnsInternalValueIfCreatedWithValue() {
     Maybe<int> maybe = new(10);
     maybe.OrElse(-1).Should().Be(10);
   }
