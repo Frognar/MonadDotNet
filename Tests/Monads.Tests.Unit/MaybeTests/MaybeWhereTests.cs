8b0f0855d5801d2c8c24c84c45401dae7a925d46
frognar <sebastian.przyszlak@gmail.com>
2024-02-03 07:41:52 +0100

    Refactor Where tests and add tests for query syntax

--- Tests/Monads.Tests.Unit/MaybeTests/MaybeWhereTests.cs
+++ Tests/Monads.Tests.Unit/MaybeTests/MaybeWhereTests.cs
@@ -1,22 +1,13 @@
-﻿using Frognar.Monads.Optional;
-
-namespace Monads.Tests.Unit.MaybeTests;
+﻿namespace Monads.Tests.Unit.MaybeTests;
 
 public class MaybeWhereTests {
-  [Fact]
-  public void ReturnsSomeWhenPredicateIsMet() {
-    Some(10)
-      .Where(x => x < 100)
+  [Theory]
+  [ClassData(typeof(Helpers.TestDataGenerators.IntFilteredBeLessThanOrEqualTo0TestData))]
+  public void FiltersSomeWithPredicate(int value, int expected) {
+    Some(value)
+      .Where(x => x <= 0)
       .OrElse(Minus1)
-      .Should().Be(10);
-  }
-
-  [Fact]
-  public void ReturnsNoneWhenPredicateIsNotMet() {
-    Some(10)
-      .Where(x => x > 10)
-      .OrElse(Minus1)
-      .Should().Be(-1);
+      .Should().Be(expected);
   }
 
   [Fact]
@@ -24,6 +15,29 @@ public class MaybeWhereTests {
     None<int>()
       .Where(x => x % 2 == 0)
       .OrElse(Minus1)
+      .Should().Be(-1);
+  }
+
+  [Theory]
+  [ClassData(typeof(Helpers.TestDataGenerators.IntFilteredBeLessThanOrEqualTo0TestData))]
+  public void FiltersValueWithQuerySyntax(int value, int expected) {
+    Maybe<int> result = 
+      from a in Some(value)
+      where a <= 0
+      select a;
+    
+    result.OrElse(Minus1)
+      .Should().Be(expected);
+  }
+
+  [Fact]
+  public void PropagateNoneRegardlessOfPredicateWithQuerySyntax() {
+    Maybe<int> result = 
+      from a in None<int>()
+      where a <= 0
+      select a;
+    
+    result.OrElse(Minus1)
       .Should().Be(-1);
   }
 }
