a1632ac5bb3510fec4a14fb7ffbf970be449d9ba
frognar <sebastian.przyszlak@gmail.com>
2024-02-04 08:42:20 +0100

    Create Either struct with Left factory method

---
+++ Tests/Monads.Tests.Unit/EitherTests/EitherCreationTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Monads.Tests.Unit.EitherTests;
+
+public class EitherCreationTests {
+  [Fact]
+  public void IsNotNullWhenLeft() {
+    Either<int, string>.Left(42).Should().NotBeNull();
+    Either<bool, int>.Left(true).Should().NotBeNull();
+    Either<string, decimal>.Left("str").Should().NotBeNull();
+  }
+}
---
+++ src/Monads/Either.cs
@@ -0,0 +1,7 @@
+﻿namespace Frognar.Monads;
+
+public readonly record struct Either<L, R> {
+  public static Either<L, R> Left(L value) {
+    return new Either<L, R>();
+  }
+}
