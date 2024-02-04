a683c50969b55a99116dbb26135f945816ecf1eb
frognar <sebastian.przyszlak@gmail.com>
2024-02-04 15:49:51 +0100

    Reorder some tests

--- Tests/Monads.Tests.Unit/EitherTests/EitherSelectTests.cs
+++ Tests/Monads.Tests.Unit/EitherTests/EitherSelectTests.cs
@@ -18,17 +18,17 @@ public class EitherSelectTests {
   }
 
   [Fact]
-  public void ThrowArgumentNullExceptionWhenLeftSelectorIsNull() {
-    Action act = () => Either<int, string>.Left(42).SelectLeft<int>(null!);
-    act.Should().Throw<ArgumentNullException>();
-  }
-
-  [Fact]
   public void PropagateRightWhenRightCreated() {
     Either<int, bool>.Right(true)
       .SelectLeft(x => x.ToString())
       .Match(left: left => left, right: right => right ? "true" : "false")
       .Should().Be("true");
+  }
+
+  [Fact]
+  public void ThrowArgumentNullExceptionWhenLeftSelectorIsNull() {
+    Action act = () => Either<int, string>.Left(42).SelectLeft<int>(null!);
+    act.Should().Throw<ArgumentNullException>();
   }
 
   [Fact]
@@ -48,17 +48,17 @@ public class EitherSelectTests {
   }
 
   [Fact]
-  public void ThrowArgumentNullExceptionWhenRightSelectorIsNull() {
-    Action act = () => Either<int, string>.Right("42").SelectRight<int>(null!);
-    act.Should().Throw<ArgumentNullException>();
-  }
-
-  [Fact]
   public void PropagateLeftWhenLeftCreated() {
     Either<int, bool>.Left(42)
       .SelectRight(x => x ? 1 : 0)
       .Match(left: _ => -1, right: right => right)
       .Should().Be(-1);
+  }
+
+  [Fact]
+  public void ThrowArgumentNullExceptionWhenRightSelectorIsNull() {
+    Action act = () => Either<int, string>.Right("42").SelectRight<int>(null!);
+    act.Should().Throw<ArgumentNullException>();
   }
 
   [Fact]
